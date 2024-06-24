9a3fb42ccf577ae56f3fa05987a1ecf2211cf047
Punit Patel <patelpunit10@gmail.com>
2024-06-24 00:37:14 -0700

    Time: 2 ms (55.29%) | Memory: 41.4 MB (55.44%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isValid(String s) {
+        boolean result = false;
+        Stack<Character> checkResult = new Stack<>();
+        int lengthOFGivenString = s.length();
+        if (lengthOFGivenString <= 1) {
+            return result;
+        }
+
+        if (s.charAt(0) == ')' || s.charAt(0) == ']' || s.charAt(0) == '}') {
+            return result;
+        }
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                checkResult.push(c);
+            } else if (c == ')' && checkResult.empty()) {
+                return result;
+            } else if (c == ']' && checkResult.empty()) {
+                return result;
+            } else if (c == '}' && checkResult.empty()) {
+                return result;
+            } else if (c == ')' && checkResult.lastElement() == '(') {
+                checkResult.pop();
+            } else if (c == ']' && checkResult.lastElement() == '[') {
+                checkResult.pop();
+            } else if (c == '}' && checkResult.lastElement() == '{') {
+                checkResult.pop();
+            } else if(c == ')' || c == ']' || c == '}') {
+                checkResult.push(c);
+            }
+
+        }
+        return checkResult.empty() ? true : false;
+    }
+}
