c473267d0ca5674de0cf7756ab15ed45d9384bc9
Punit Patel <patelpunit10@gmail.com>
2024-06-25 15:28:07 -0700

    Time: 13 ms (30.79%) | Memory: 44.6 MB (32.42%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -6,14 +6,9 @@ class Solution {
             for (int j = 0; j < 9; j++) {
                 if (board[i][j] != '.') {
                     boolean rowCheck = set.add(board[i][j] + " found at row: " + i);
-                    if (!rowCheck || !set.add(board[i][j] + " found at column: " + j)
-                            || !set.add(board[i][j] + " found at box: [ " + i / 3 + " ," + j / 3 + "]"))
-                        // System.out.println(board[i][j]+ " found at row: " +i);
-                        // System.out.println(board[i][j]+ " found at column: " +j);
-                        // System.out.println(board[i][j]+ " found at box: [ " +i/3+ " ," +j/3 + "]");
-                        // set.add(board[i][j]+ " found at row: " +i);
-                        // set.add(board[i][j]+ " found at column: " +j);
-                        // set.add(board[i][j]+ " found at box: [ " +i/3+ " ," +j/3 + "]");
+                    boolean columnCheck = set.add(board[i][j] + " found at column: " + j);
+                    boolean boxCheck = set.add(board[i][j] + " found at box: [ " + i / 3 + " ," + j / 3 + "]");
+                    if (!rowCheck || !columnCheck || !boxCheck)
                         return false;
                 }
             }
@@ -22,3 +17,18 @@ class Solution {
         return result;
     }
 }
+
+/*
+ * NOTE:
+ * 
+ * HashSet.add()-> when duplicate string comes, it can't add and return false
+ * That's why I checked here: if (!rowCheck || !columnCheck || !boxCheck)
+ * 
+ * System.out.println(board[i][j]+ " found at row: " +i);
+ * System.out.println(board[i][j]+ " found at column: " +j);
+ * System.out.println(board[i][j]+ " found at box: [ " +i/3+ " ," +j/3 + "]");
+ * set.add(board[i][j]+ " found at row: " +i);
+ * set.add(board[i][j]+ " found at column: " +j);
+ * set.add(board[i][j]+ " found at box: [ " +i/3+ " ," +j/3 + "]");
+ * 
+ */
