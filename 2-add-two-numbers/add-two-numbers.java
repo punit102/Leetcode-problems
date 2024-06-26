39198ea738ba39775247f146b4b5767898810971
Punit Patel <patelpunit10@gmail.com>
2024-06-25 18:17:13 -0700

    Time: 1 ms (100.00%) | Memory: 44.8 MB (11.16%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.java
@@ -0,0 +1,52 @@
+
+
+import javax.smartcardio.Card;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy_node = new ListNode();
+        ListNode cursor = dummy_node;
+        int carry = 0;
+        // int total = 0;
+
+        while(l1 != null || l2 != null || carry > 0) {
+            int v1 = 0;
+            int v2 = 0;
+             
+            if (l1 != null){
+                v1 = l1.val;
+            }
+
+            if (l2 != null){
+                v2 = l2.val;
+            }
+            
+            int total = v1 + v2 + carry; 
+            carry = total / 10;
+            total = total % 10;
+            cursor.next = new ListNode(total);
+
+            // Update pointers
+            cursor = cursor.next;
+            if (l1 != null){
+               l1 = l1.next;
+            }
+
+            if (l2 != null){
+               l2 = l2.next;
+            }
+        }
+
+    return dummy_node.next;       
+    }
+}
