48f7a2ea22a1dee001417004660d13d25b7a09c7
Punit Patel <patelpunit10@gmail.com>
2024-07-06 19:24:50 -0700

    Time: 29 ms (8.59%) | Memory: 76 MB (7.51%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.java
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.java
@@ -2,24 +2,34 @@ class Solution {
     public int findCenter(int[][] edges) {
         int result = 0;
         int no_of_edges = edges.length;
-        System.out.println("No of Edges: " + no_of_edges);
-        HashMap<Integer, Integer> repeat_of_no = new HashMap<>();
+        HashMap<Integer, Integer> check_repeat = new HashMap<>();
 
-        for (int i = 0; i < no_of_edges; i++) {
-            for (int j = 0; j < 2; j++) {
-                if (!repeat_of_no.containsKey(edges[i][j])) {
-                    repeat_of_no.put(edges[i][j], 1);
-                } else {
-                    int temp_count = repeat_of_no.get(edges[i][j]);
-                    repeat_of_no.put(edges[i][j], temp_count + 1);
-                }
-            }
+        // for (int i = 0; i < no_of_edges; i++) {
+        // for (int j = 0; j < 2; j++) {
+        // if (!repeat_of_no.containsKey(edges[i][j])) {
+        // repeat_of_no.put(edges[i][j], 1);
+        // } else {
+        // int temp_count = repeat_of_no.get(edges[i][j]);
+        // repeat_of_no.put(edges[i][j], temp_count + 1);
+        // }
+        // }
+        // }
+
+        // for (Map.Entry<Integer, Integer> entry : repeat_of_no.entrySet()) {
+        // if (entry.getValue() == no_of_edges)
+        // result = entry.getKey();
+        // }
+
+        for (int[] arr : edges) {
+            check_repeat.put(arr[0], check_repeat.getOrDefault(arr[0], 0) + 1);
+            check_repeat.put(arr[1], check_repeat.getOrDefault(arr[1], 0) + 1);
         }
 
-        for (Map.Entry<Integer, Integer> entry : repeat_of_no.entrySet()) {
-            if (entry.getValue() == no_of_edges)
-                result = entry.getKey();
+        for (int key : check_repeat.keySet()) {
+            if (check_repeat.get(key) == no_of_edges)
+                result = key;
         }
+
         return result;
     }
 }
