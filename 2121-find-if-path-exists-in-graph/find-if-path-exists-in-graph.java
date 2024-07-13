8dec087ed124db61dbc9d98d0b55e56d6de6c0dc
Punit Patel <patelpunit10@gmail.com>
2024-07-13 13:34:12 -0700

    Time: 173 ms (35.21%) | Memory: 182.9 MB (25.33%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,38 +1,39 @@
-
 import static java.util.Collections.addAll;
 
 class Solution {
-    boolean visited[];
-    List<List<Integer>> gr;
 
-    void dfs(int node) {
-        if (visited[node])
-            return;
-        visited[node] = true;
+    boolean[] visited;
+    List<ArrayList<Integer>> eachNodeconnectedEdges = new ArrayList<ArrayList<Integer>>();
 
-        for (int child : gr.get(node)) {
-            if (!visited[child]) {
-                dfs(child);
-            }
-        }
-    }
-
+    // DFS way implementation
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        visited = new boolean[n];
-        gr = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
-            gr.add(new ArrayList<>());
+        visited = new boolean[n];
+
+        for (int i =0; i<n; i++){
+            eachNodeconnectedEdges.add(new ArrayList<Integer>());
         }
 
-        for (int[] edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            gr.get(u).add(v);
-            gr.get(v).add(u);
+        for (int[] edge: edges) {
+            int a = edge[0];
+            int b = edge[1];
+            eachNodeconnectedEdges.get(a).add(b);
+            eachNodeconnectedEdges.get(b).add(a);
         }
 
         dfs(source);
         return visited[destination];
     }
+
+    private void dfs(int source){
+        if(visited[source]) return;
+        visited[source] = true;
+
+        for (int coonectedNode: eachNodeconnectedEdges.get(source)){
+            if (!visited[coonectedNode]){
+                dfs(coonectedNode);
+            }
+        }
+    }
+
 }
