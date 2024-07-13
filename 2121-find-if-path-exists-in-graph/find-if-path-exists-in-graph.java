219fc06398a5c9305a9cdceb4d6d0c23608ded89
Punit Patel <patelpunit10@gmail.com>
2024-07-13 14:35:32 -0700

    Time: 162 ms (40.06%) | Memory: 182.4 MB (25.77%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -3,7 +3,7 @@ import static java.util.Collections.addAll;
 class Solution {
 
     boolean[] visited;
-    List<ArrayList<Integer>> eachNodeconnectedEdges = new ArrayList<ArrayList<Integer>>();
+    List<ArrayList<Integer>> listForEachNodeToConnectedNodes = new ArrayList<ArrayList<Integer>>();
 
     // DFS way implementation
     public boolean validPath(int n, int[][] edges, int source, int destination) {
@@ -11,14 +11,14 @@ class Solution {
         visited = new boolean[n];
 
         for (int i =0; i<n; i++){
-            eachNodeconnectedEdges.add(new ArrayList<Integer>());
+            listForEachNodeToConnectedNodes.add(new ArrayList<Integer>());
         }
 
         for (int[] edge: edges) {
             int a = edge[0];
             int b = edge[1];
-            eachNodeconnectedEdges.get(a).add(b);
-            eachNodeconnectedEdges.get(b).add(a);
+            listForEachNodeToConnectedNodes.get(a).add(b);
+            listForEachNodeToConnectedNodes.get(b).add(a);
         }
 
         dfs(source);
@@ -29,7 +29,7 @@ class Solution {
         if(visited[source]) return;
         visited[source] = true;
 
-        for (int coonectedNode: eachNodeconnectedEdges.get(source)){
+        for (int coonectedNode: listForEachNodeToConnectedNodes.get(source)){
             if (!visited[coonectedNode]){
                 dfs(coonectedNode);
             }
@@ -37,3 +37,16 @@ class Solution {
     }
 
 }
+
+
+// // BFS way
+// class Solution {
+
+
+//     // BFS way implementation
+//     public boolean validPath(int n, int[][] edges, int source, int destination) {
+
+//         return false;
+//     }
+
+// }
