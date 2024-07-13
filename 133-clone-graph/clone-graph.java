b1930fb616687ec6a7db7a82235ec5b7838a793a
Punit Patel <patelpunit10@gmail.com>
2024-07-13 15:52:48 -0700

    Time: 24 ms (95.06%) | Memory: 42.6 MB (64.14%) - LeetSync

--- 133-clone-graph/clone-graph.java
+++ 133-clone-graph/clone-graph.java
@@ -18,6 +18,8 @@ class Node {
 }
 */
 
+/*
+// BFS solution
 class Solution {
     public Node cloneGraph(Node node) {
 
@@ -42,5 +44,28 @@ class Solution {
         }
 
     return map.get(node);    
+    }
+}
+*/
+
+
+// DFS Solution
+class Solution {
+    private HashMap<Node, Node> visited = new HashMap<>() ;
+
+    public Node cloneGraph(Node node) {
+
+        if(node == null) return node;
+
+        if(visited.containsKey(node)) return visited.get(node);
+
+        Node cloneNode = new Node(node.val, new ArrayList<>());
+        visited.put(node, cloneNode);
+
+        for(Node neighbor: node.neighbors){
+            cloneNode.neighbors.add(cloneGraph(neighbor));
+        }
+
+    return cloneNode;
     }
 }
