6b382cf9085e28f08ac8f53df91695597e9ebe79
Punit Patel <patelpunit10@gmail.com>
2024-07-26 12:19:54 -0700

    Time: 84 ms (12.79%) | Memory: 59.9 MB (73.23%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -1,21 +1,30 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
+        // initialize the result array and
+        // monolithic stack(which only store decreasing elements otherwise start poping
+        // out the element)
         int[] result = new int[temperatures.length];
         Stack<Integer> monoStack = new Stack<>();
 
-        for(int i = temperatures.length - 1; i >= 0; i--){
+        // Start for loop from last Index of the array
+        for (int i = temperatures.length - 1; i >= 0; i--) {
 
-            while(!monoStack.isEmpty() && temperatures[i] >= temperatures[monoStack.peek()]){
+            // if current index temperature is higher thatn stack top() element then we
+            // can't push it so we will poping untill smaller elements in the stack top or
+            // wait untill the stack will empty
+            while (!monoStack.isEmpty() && temperatures[i] >= temperatures[monoStack.peek()]) {
                 monoStack.pop();
             }
 
-            if(!monoStack.isEmpty()){
+            // After popping out all necessary elements - we do subtraction on stack pop
+            // index - current array index
+            if (!monoStack.isEmpty()) {
                 result[i] = monoStack.peek() - i;
             }
 
+            // push all th array elements index to stack
             monoStack.push(i);
         }
-
 
         return result;
     }
