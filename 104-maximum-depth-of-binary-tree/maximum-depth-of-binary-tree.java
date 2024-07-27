5c71380aa1d719c8f750f7c38bd1c36ee34e937c
Punit Patel <patelpunit10@gmail.com>
2024-07-27 16:17:39 -0700

    Time: 0 ms (100.00%) | Memory: 42.4 MB (54.01%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -15,17 +15,50 @@ import static java.lang.Math.max;
  * this.right = right;
  * }
  * }
- */
+//  */
+// class Solution {
+
+//     public int maxDepth(TreeNode root) {
+//         if (root == null) {
+//             return 0;
+//         }
+
+//         int leftNode = maxDepth(root.left);
+//         int rightNode = maxDepth(root.right);
+//         return Math.max(leftNode, rightNode) + 1;
+//     }
+
+// }
+
 class Solution {
-
     public int maxDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
+        if(root == null) return 0;
+        int leftCount = maxDepth(root.left) + 1;
+        int rightCount = maxDepth(root.right) + 1;
 
-        int leftNode = maxDepth(root.left);
-        int rightNode = maxDepth(root.right);
-        return Math.max(leftNode, rightNode) + 1;
+        return Math.max(leftCount, rightCount);
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
