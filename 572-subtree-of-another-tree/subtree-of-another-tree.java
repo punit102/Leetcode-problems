f897a658bcadc6a938a8a5880491d7f6f282bbff
Punit Patel <patelpunit10@gmail.com>
2024-07-29 14:44:22 -0700

    Time: 2 ms (96.81%) | Memory: 43.8 MB (97.28%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -1,32 +1,69 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
+// class Solution {
+//     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+//         if (root == null) {
+//             return subRoot == null;
+//         }
+
+//         if (checkSameTree(root, subRoot))
+//             return true;
+
+//         return checkSameTree(root.left, subRoot) || checkSameTree(root.right, subRoot);
+//     }
+
+//     private boolean checkSameTree(TreeNode s, TreeNode t) {
+
+//         if (s == null && t == null)
+//             return true;
+
+//         if (s == null || t == null)
+//             return false;
+
+//         if (s.val != t.val)
+//             return false;
+
+//         return checkSameTree(s.left, t.left) && checkSameTree(s.right, t.right);
+//     }
+// }
+
+
+
 class Solution {
-    public boolean isSubtree(TreeNode s, TreeNode t) {
-      return  s!=null && (validateSubtree(s,t) || isSubtree(s.left,t) || isSubtree(s.right,t));
-    }
-    
-    
-    public boolean validateSubtree(TreeNode s, TreeNode t){
-        if(s == null && t == null){
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root==null){
+            return subRoot==null;
+        }
+        if(isSametree(root,subRoot)){
             return true;
         }
-        if(s == null || t == null){
+        return isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot);
+                
+    }
+    boolean isSametree(TreeNode p, TreeNode q){
+        if(p==null && q==null){
+            return true;
+        }
+        if(p==null ||  q==null){
             return false;
         }
-        
-        return s.val == t.val && validateSubtree(s.left, t.left) && validateSubtree(s.right,t.right);
+        if(p.val!=q.val){
+            return false;
+        }
+        return isSametree(p.left ,q.left) && isSametree(p.right,q.right);
+
     }
 }
